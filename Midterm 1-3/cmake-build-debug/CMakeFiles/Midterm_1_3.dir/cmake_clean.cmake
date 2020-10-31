923b1937f6aebdc62d43705710ca2584f267bc21
Jerry-Wan <63923979+Jerry-Wan@users.noreply.github.com>
2020-10-31 15:32:55 +0800

    Add files via upload

---
+++ Midterm 1-3/cmake-build-debug/CMakeFiles/Midterm_1_3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Midterm_1_3.dir/main.c.obj"
+  "Midterm_1_3.exe"
+  "Midterm_1_3.exe.manifest"
+  "Midterm_1_3.lib"
+  "Midterm_1_3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Midterm_1_3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Midterm 1-3/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char *mcopy(char*s,int n){
+    int len = 0;
+    while (*(s+len) != 0){
+        len++;
+    }
+    char *head = malloc(len*n*sizeof(char));
+    char *result = head;
+    int a = 0;
+    int b = 0;
+    char *p;
+    for (a = 0; a < n; a++){
+        for (b = 0; b < n; b++){
+            int c =  a+b;
+            *head = strtoul(c, &p,10);;
+            head++;
+        }
+
+    }
+    *head = 0;
+    return result;
+}
+
+int main(){
+    char s = "123def”";
+    printf("%s",mcopy(s,3));
+    return 0;
+}
